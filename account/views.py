25c5f0085ea6a5f95f0b7faa886de9f6081619bf
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-02-08 23:38:32 +0500

    Complated Account

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import *
 from account.models import *
+import os
 
 
 def signUpView(request):
@@ -71,6 +72,39 @@ def justUser(request):
     context = {}
     context['request_user'] = request.user
     return render(request,'user_detail.html',context)
+
+@login_required
+def justUserDetail(request,pk):
+    context = {}
+    context['user'] = User.objects.filter(id = pk)[0]
+    context['gender'] = Gender.objects.all()
+    if request.method=='POST':
+        if len(request.FILES) != 0:
+            if len(context['user'].photo) > 0:
+                os.remove(context['user'].photo.url)
+                photo = request.FILES['photo']
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        phone_number = request.POST.get('phone_number')
+        gender = Gender.objects.get(id = request.POST.get('gender'))
+        birthdate = request.POST.get('birthdate')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+               
+        if first_name == "" or last_name == "" or phone_number == "" or birthdate == "" or photo == None:
+            context['error'] = "Информация неполная"
+            return render(request,'user_edit.html',context) 
+        
+        context['user'].first_name = first_name
+        context['user'].last_name = last_name
+        context['user'].phone_number = phone_number
+        context['user'].gender = gender
+        context['user'].birthdate = birthdate
+        context['user'].set_password(password1)
+        context['user'].username = username
+        context['user'].save()
+    return render(request,'user_edit.html',context) 
 
 @login_required
 def logoutView(request):
